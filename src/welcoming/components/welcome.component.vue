91f920cfcd53cd9de77e9fcf577033bd91580233
Angel Velasquez <pcisavel@upc.edu.pe>
2024-08-29 16:28:55 -0500

    feat(welcoming): added welcome component.

---
+++ src/welcoming/components/welcome.component.vue
@@ -0,0 +1,26 @@
+<script>
+import {Developer} from "../model/developer.entity.js";
+
+export default {
+  name: "welcome",
+  props: {
+    firstName: String,
+    lastName: String
+  },
+  methods: {
+    developerName() {
+      let developer = new Developer(this.firstName, this.lastName);
+      return developer.fullName;
+    }
+  }
+
+}
+</script>
+
+<template>
+  <p>Hello {{ developerName }}. Now You are a Vue Developer.</p>
+</template>
+
+<style scoped>
+
+</style>
